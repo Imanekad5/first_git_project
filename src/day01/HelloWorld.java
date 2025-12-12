465e2e093f05f69030b35cd6d150e87573887bd9
Imanekad5 <imane.kaddouri5@outlook.com>
2025-12-12 12:01:33 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,9 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
+
+
     }
 }
